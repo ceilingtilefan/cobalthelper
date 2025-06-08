29bc79b6e9e632f7660336d88ded4a1763e97813
bigfloppa234 <104149053+ceilingtilefan@users.noreply.github.com>
2025-06-08 13:43:30 -0500

    Initial bot file commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # cobalthelper
 Save what you love - all in Discord
+
+# supported services
+TikTok
+Instagram
+Twitter/X
+Reddit
+BlueSky
+FaceBook
+SnapChat
+Tumblr
+Twitch Clips
+
+# setup
+Requirements:
+- A Discord bot (you can create one [here](https://discord.com/developers/applications)) (note: make sure you have the message content intent enabled under the bot tab!)
+- Docker
+
+1. Get your bot token and place it in the Docker compose file
+2. Open a terminal in the bot folder and run `docker compose up` (or `docker compose up -d` to run it in the background)
+
+-# this project is not affiliated with the imput team nor Discord.
---
+++ bot.py
@@ -0,0 +1,46 @@
+import discord # type: ignore
+import os
+import requests # type: ignore
+from urlextract import URLExtract # type: ignore
+from urllib.parse import urlparse
+import json
+import io
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user} using Discord.py version {discord.__version__}')
+
+    async def on_message(self, message):
+        supportedURL = [
+            "www.tiktok.com",
+            "vm.tiktok.com",
+            "www.instagram.com"
+            "twitter.com",
+            "x.com",
+            "www.reddit.com",
+            "bsky.app",
+            "www.facebook.com",
+            "www.snapchat.com",
+            "www.tumblr.com",
+            "www.twitch.tv"
+        ]
+
+        urls = URLExtract().find_urls(message.content)
+        print(urls)
+        if urls:
+            url = urlparse(urls[0]).hostname
+            print(url)
+            if url in supportedURL:
+                cobalt = requests.post('http://192.168.1.5:9000', headers = {"Accept": "application/json", "Content-Type": "application/json"}, json={"url": str(urls[0])})
+                videoUrl = requests.get(json.loads(cobalt.content)['url'])
+                videoToSend = io.BytesIO(videoUrl.content)
+                await message.reply(file=discord.File(videoToSend, 'video.mp4'), mention_author=False)
+                await message.edit(suppress=True)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+
+token = os.getenv('TOKEN')
+client.run(token)
