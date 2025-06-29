f1458b0febe8eb89c223024b61882783c0159b86
bigfloppa234 <104149053+ceilingtilefan@users.noreply.github.com>
2025-06-29 18:17:08 -0500

    Add spoiler and multi-attachment support

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ class MyClient(discord.Client):
         supportedURL = [
             "www.tiktok.com",
             "vm.tiktok.com",
+            "vt.tiktok.com",
             "www.instagram.com",
             "twitter.com",
             "x.com",
@@ -26,13 +27,33 @@ class MyClient(discord.Client):
         ]
 
         urls = URLExtract().find_urls(message.content)
+        print(urls)
         if urls:
             url = urlparse(urls[0]).hostname
+            print(url)
             if url in supportedURL:
-                cobalt = requests.post('http://cobalt-api:9000', headers = {"Accept": "application/json", "Content-Type": "application/json"}, json={"url": str(urls[0])})
-                videoUrl = requests.get(json.loads(cobalt.content)['url'])
-                videoToSend = io.BytesIO(videoUrl.content)
-                await message.reply(file=discord.File(videoToSend, 'video.mp4'), mention_author=False)
+                cobalt = requests.post('http://cobalt-api:9000', headers = {"Accept": "application/json", "Content-Type": "application/json"}, json={"url": str(urls[0]).split('||')[0]})
+                cobaltJson = json.loads(cobalt.content)
+                mediaToSend = []
+                if "||" in message.content:
+                    spoiler = True
+                else:
+                    spoiler = False
+                if cobaltJson['status'] == 'picker':
+                    for each in cobaltJson['picker']:
+                        mediaUrl = requests.get(each['url']) 
+                        orig_filename = each['url'].split('/')[-1].split('?')[0]
+                        filename = f"SPOILER_{orig_filename}" if spoiler else orig_filename
+                        media = io.BytesIO(mediaUrl.content)
+                        mediaToSend.append(discord.File(media, filename=filename))
+                else:
+                    print(cobaltJson)
+                    mediaUrl = requests.get(cobaltJson['url'])
+                    media = io.BytesIO(mediaUrl.content)
+                    orig_filename = cobaltJson['filename']
+                    filename = f"SPOILER_{orig_filename}" if spoiler else orig_filename
+                    mediaToSend.append(discord.File(media, filename=filename))
+                await message.reply(files=mediaToSend, mention_author=False) 
                 await message.edit(suppress=True)
 
 intents = discord.Intents.default()
